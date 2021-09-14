4e9d5fc31e0dca1e3b328502fe939e1134e252d2
Christian Yngvesson <chrille_y@hotmail.com>
2021-09-14 16:36:54 +0200

    Login + session saving implemented.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+
+
 const main = document.getElementById('main');
 const login = document.getElementById('login');
-const nameInput = document.getElementById('nameInput');
-const passInput = document.getElementById('passInput');
-
+const nameInput = document.getElementById('nameInput')
+const passInput = document.getElementById('passInput')
 const loginButton = document.getElementById('loginButton');
 
 loginButton.addEventListener('click', function () {
-    if (nameInput === "test" && passInput ==="1234") {
+
+    if (nameInput.value === "test" && passInput.value === "1234") {
+        localStorage.setItem('session', true)
         login.classList.add('hide')
         pageSuccess();
     } else {
@@ -15,22 +18,66 @@ loginButton.addEventListener('click', function () {
     }
 })
 
-if (localStorage.getItem('session')) {
-    pageSuccess()
-    console.log("success!");
-} else {
-    login.classList.remove('hide');
-    console.log("go to login");
+function checkSession() {
+    if (localStorage.getItem('session')) {
+        pageSuccess()
+    } else {
+        login.classList.remove('hide');
+    }
+}
+
+// Template page for us to mold...
+const page = {
+    div: document.createElement('div'),
+    title: document.createElement('h3'),
+    text: document.createElement('p'),
+    button: document.createElement('button')
 }
 
 function pageSuccess() {
-    const success = document.createElement('success');
-    success.classList.add('container');
-    document.getElementById('main.id').appendChild('success')
+    page.div.id = "success";
+    page.title.textContent = "You're in!"
+    page.text.textContent = "Welcome to the the best site - Enjoy your stay."
+    page.button.textContent = "Sign out"
+
+    page.button.addEventListener('click', function () {
+        main.removeChild(page.div);
+        login.classList.remove('hide');
+        localStorage.removeItem('session')
+    })
+
+    main.appendChild(page.div);
+    page.div.appendChild(page.title);
+    page.div.appendChild(page.text);
+    page.div.appendChild(page.button);
 }
 
 function pageFail() {
-    const fail = document.createElement('fail');
-    fail.classList.add('container');
-    document.getElementById('main.id').appendChild('fail')
+    page.div.id = "fail";
+    page.title.textContent = "Whoops!"
+    page.text.textContent = "Incorrect credentials: Terminator monkeys has been deployed."
+    page.button.textContent = "Try again"
+
+    page.button.addEventListener('click', function () {
+        main.removeChild(page.div);
+        login.classList.remove('hide');
+    })
+    main.appendChild(page.div);
+    page.div.appendChild(page.title);
+    page.div.appendChild(page.text);
+    page.div.appendChild(page.button);
 }
+
+checkSession();
+
+
+/*
+
+for (const [key, value] of Object.entries(fail)) {
+        console.log(`${key}: ${value}`);
+        main.appendChild(fail[key])
+      }
+
+    //appendAll(fail.array);
+
+    */
