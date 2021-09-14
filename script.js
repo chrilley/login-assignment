163c14cc9a5e41be2edc8cad95d56e69f7baf479
Christian Yngvesson <chrille_y@hotmail.com>
2021-09-14 20:57:19 +0200

    Fixed input boxes sticking out from their containing element.

--- script.js
+++ script.js
@@ -1,74 +1,80 @@
 // Declare some important elements
 const main = document.getElementById('main');
 const login = document.getElementById('login');
-const nameInput = document.getElementById('nameInput')
-const passInput = document.getElementById('passInput')
+const nameInput = document.getElementById('nameInput');
+const passInput = document.getElementById('passInput');
 const loginButton = document.getElementById('loginButton');
 
-// And a template to create pages from
-const page = {
-    div: document.createElement('div'),
-    title: document.createElement('h3'),
-    text: document.createElement('p'),
-    button: document.createElement('button')
+// Constructor for our website panels.
+function Panel(div, title, text, button) {
+ /*   this.div = div;
+    this.title = title;
+    this.text = text;
+    this.button = button;*/
+    this.create = function () {
+        const newDiv = document.createElement('div');
+        newDiv.id = div;
+        document.getElementById(main.id).appendChild(newDiv);
+
+        const newTitle = document.createElement('h3');
+        newTitle.textContent = title;
+        document.getElementById(newDiv.id).appendChild(newTitle);
+
+        const newText = document.createElement('p');
+        newText.textContent = text;
+        document.getElementById(newDiv.id).appendChild(newText);
+
+        const newButton = document.createElement('button');
+        newButton.textContent = button;
+        document.getElementById(newDiv.id).appendChild(newButton);
+
+        newButton.addEventListener('click', this.action);
+    };
+
+    this.action = function () {
+
+        login.classList.remove('hide');
+        document.getElementById('success').classList.add('hide');
+        document.getElementById('fail').classList.add('hide');
+
+        if (this.button = 'Sign Out') {
+            login.classList.remove('hide');
+            localStorage.removeItem('session');
+        }
+    }
 }
 
-loginButton.addEventListener('click', function () {
-    if (nameInput.value === "test" && passInput.value === "1234") {
-        localStorage.setItem('session', true)
-        login.classList.add('hide')
-        pageSuccess();
-    } else {
-        login.classList.add('hide')
-        pageFail();
-    }
-})
+// Create the required panels using our constructor
+const successPanel = new Panel('success', 'You\'re in!', 'Welcome to the best site - Enjoy your stay!', 'Sign Out');
+const failPanel = new Panel('fail', 'Whoops!', 'Wrong credentials: Terminator monkeys has been deployed.', 'Try Again');
 
-// If anyone is logged in, go to "success" page else show login form
+failPanel.create();
+successPanel.create();
+
 function checkSession() {
     if (localStorage.getItem('session')) {
-        pageSuccess()
+        login.classList.add('hide');
+        document.getElementById('success').classList.remove('hide')
+        document.getElementById('fail').classList.add('hide');
     } else {
         login.classList.remove('hide');
+        document.getElementById('success').classList.add('hide');
+        document.getElementById('fail').classList.add('hide');
     }
-}
-
-// Creates the success page
-function pageSuccess() {
-    page.div.id = "success";
-    page.title.textContent = "You're in!"
-    page.text.textContent = "Welcome to the the best site - Enjoy your stay."
-    page.button.textContent = "Sign out"
-
-    main.appendChild(page.div);
-    page.div.appendChild(page.title);
-    page.div.appendChild(page.text);
-    page.div.appendChild(page.button);
-
-    page.button.addEventListener('click', function () {
-        main.removeChild(page.div);
-        login.classList.remove('hide');
-        localStorage.removeItem('session')
-    })
-}
-
-// Creates the fail page
-function pageFail() {
-    page.div.id = "fail";
-    page.title.textContent = "Whoops!"
-    page.text.textContent = "Incorrect credentials: Terminator monkeys has been deployed."
-    page.button.textContent = "Try again"
-
-    page.button.addEventListener('click', function () {
-        main.removeChild(page.div);
-        login.classList.remove('hide');
-    })
-
-    main.appendChild(page.div);
-    page.div.appendChild(page.title);
-    page.div.appendChild(page.text);
-    page.div.appendChild(page.button);
 }
 
 // Look for user on page-load
 checkSession();
+
+//Login Button
+loginButton.addEventListener('click', function () {
+
+    if (nameInput.value === "test" && passInput.value === "1234") {
+        localStorage.setItem('session', true)
+        login.classList.add('hide')
+        document.getElementById('success').classList.remove('hide');
+    } else {
+        login.classList.add('hide');
+        document.getElementById('fail').classList.remove('hide');
+    }
+});
